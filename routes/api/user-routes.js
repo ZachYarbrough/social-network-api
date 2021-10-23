650b91c3c0915ce2a01c5e1339195129d53de6bf
Zach Yarbrough <zachyarbro@gmail.com>
2021-10-23 10:38:26 -0500

    add user and friend routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,76 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.get('/', (req, res) => {
+    User.find()
+        .then(userData => res.json(userData))
+        .catch(err => res.json(err));
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({ _id: req.params.id })
+        .then(userData => {
+            if (!userData) {
+                res.json(404).status({ message: 'No user with that id.' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => res.json(err));
+});
+
+router.post('/', (req, res) => {
+    User.create(req.body)
+        .then(userData => res.json(userData))
+        .catch(err => res.json(err));
+});
+
+router.put('/:id', (req, res) => {
+    User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+        .then(userData => {
+            if (!userData) {
+                res.json(404).status({ message: 'No user with that id.' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => res.json(err));
+});
+
+router.delete('/:id', (req, res) => {
+    User.findOneAndDelete({ _id: req.params.id }, { $pull: { thoughts } }, { new: true })
+        .then(userData => {
+            if (!userData) {
+                res.json(404).status({ message: 'No user with that id.' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => res.json(err));
+});
+
+router.post('/:userId/friends/:friendId', (req, res) => {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $push: { friends: req.params.friendId } }, { new: true })
+        .then(friendData => {
+            if (!friendData) {
+                res.json(404).status({ message: 'No user with that id.' });
+                return;
+            }
+            res.json(friendData);
+        })
+        .catch(err => res.json(err));
+});
+
+router.delete('/:userId/friends/:friendId', (req, res) => {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true })
+        .then(userData => {
+            if (!userData) {
+                res.json(404).status({ message: 'No user with that id.' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => res.json(err));
+});
+
+module.exports = router;
