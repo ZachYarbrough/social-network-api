bfef89162e705b0e3abfb43054eed9f0dfcb1b52
Zach Yarbrough <zachyarbro@gmail.com>
2021-10-23 11:04:06 -0500

    add thought and reaction routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,96 @@
+const router = require('express').Router();
+const { User, Thought } = require('../../models');
+
+router.get('/', (req, res) => {
+    Thought.find({})
+        .then(thoughtData => res.json(thoughtData))
+        .catch(err => res.json(err));
+});
+
+router.get('/:id', (req, res) => {
+    Thought.findOne({ _id: req.params.id })
+        .then(thoughtData => {
+            if(!thoughtData) {
+                res.status(404).json({ message: 'No thought with that id.' });
+                return;
+            }
+            res.json(thoughtData);
+        })
+        .catch(err => res.json(err));
+});
+
+router.post('/', (req, res) => {
+    Thought.create(req.body)
+        .then(thoughtData => {
+            if(!thoughtData) {
+                res.status(404).json({ message: 'No thought with that id.' });
+                return;
+            }
+            return User.findOneAndUpdate({ _id: req.body.userId }, { $push: { thoughts : thoughtData._id } }, { new: true })
+        })
+        .then(userData => {
+            if(!userData) {
+                res.status(404).json({ message: 'No user with that id.' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => res.json(err));
+});
+
+router.put('/:id', (req, res) => {
+    Thought.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+        .then(thoughtData => {
+            if(!thoughtData) {
+                res.status(404).json({ message: 'No thought with that id.' });
+                return;
+            }
+            res.json(thoughtData);
+        })
+        .catch(err => res.json(err));
+});
+
+router.delete('/:id', (req, res) => {
+    Thought.findOneAndDelete({ _id: req.params.id })
+        .then(thoughtData => {
+            if(!thoughtData) {
+                res.status(404).json({ message: 'No thought with that id.' });
+                return;
+            }
+            return User.findOneAndUpdate({ _id: req.body.userId }, { $pull: { thoughts : thoughtData._id } }, { new: true })
+        })
+        .then(userData => {
+            if(!userData) {
+                res.status(404).json({ message: 'No user with that id.' });
+                return;
+            }
+            res.json(userData);
+        })
+        .catch(err => res.json(err));
+});
+
+router.post('/:thoughtId/reactions', (req, res) => {
+    Thought.findOneAndUpdate({ _id: req.params.thoughtId}, { $push: {reactions: req.body } }, { new: true })
+        .then(thoughtData => {
+            if(!thoughtData) {
+                res.status(404).json({ message: 'No thought with that id.' });
+                return;
+            }
+            res.json(thoughtData);
+        })
+        .catch(err => res.json(err));
+});
+
+router.post('/:thoughtId/reactions', (req, res) => {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId}, { $pull: {reactions: req.body } })
+        .then(thoughtData => {
+            if(!thoughtData) {
+                res.status(404).json({ message: 'No thought with that id.' });
+                return;
+            }
+            res.json(thoughtData);
+        })
+        .catch(err => res.json(err));
+});
+
+module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 router.get('/', (req, res) => {
-    User.find()
+    User.find({})
         .then(userData => res.json(userData))
         .catch(err => res.json(err));
 });
@@ -11,7 +11,7 @@ router.get('/:id', (req, res) => {
     User.findOne({ _id: req.params.id })
         .then(userData => {
             if (!userData) {
-                res.json(404).status({ message: 'No user with that id.' });
+                res.status(404).json({ message: 'No user with that id.' });
                 return;
             }
             res.json(userData);
@@ -29,7 +29,7 @@ router.put('/:id', (req, res) => {
     User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
         .then(userData => {
             if (!userData) {
-                res.json(404).status({ message: 'No user with that id.' });
+                res.status(404).json({ message: 'No user with that id.' });
                 return;
             }
             res.json(userData);
@@ -38,10 +38,10 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-    User.findOneAndDelete({ _id: req.params.id }, { $pull: { thoughts } }, { new: true })
+    User.findOneAndDelete({ _id: req.params.id }, { new: true })
         .then(userData => {
             if (!userData) {
-                res.json(404).status({ message: 'No user with that id.' });
+                res.status(404).json({ message: 'No user with that id.' });
                 return;
             }
             res.json(userData);
@@ -53,7 +53,7 @@ router.post('/:userId/friends/:friendId', (req, res) => {
     User.findOneAndUpdate({ _id: req.params.userId }, { $push: { friends: req.params.friendId } }, { new: true })
         .then(friendData => {
             if (!friendData) {
-                res.json(404).status({ message: 'No user with that id.' });
+                res.status(404).json({ message: 'No friend with that id.' });
                 return;
             }
             res.json(friendData);
@@ -65,7 +65,7 @@ router.delete('/:userId/friends/:friendId', (req, res) => {
     User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true })
         .then(userData => {
             if (!userData) {
-                res.json(404).status({ message: 'No user with that id.' });
+                res.status(404).json({ message: 'No user with that id.' });
                 return;
             }
             res.json(userData);
